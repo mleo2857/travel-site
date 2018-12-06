a83897a3a23d1ca3da51f407173d707d66b7e75c
Michael Leo <mleo2857@gmail.com>
2018-12-06 16:23:24 -0500

    first commit - basic homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.jpeg';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <body className="App-body">
+          <div className = "Homepage-msg">
+            <h2>Welcome to my travel site. Select a country to start planning your trip:</h2>
+            Country:<input type="text" name="Country"></input>
+            <input type="submit"/>
+
+            <h5>Returning User? <button>login</button></h5>
+          </div>
+        </body>
       </div>
     );
   }
---
+++ src/UserPage.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class UserPage extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, this is your user page!</h1>
+
+        <h2> Your trips:</h2>
+          <ul>
+            <li>Southwest U.S. Hiking Tour</li>
+            <li>Germany Octoberfest</li>
+            <li>Scotland/Ireland Tour</li>
+          </ul>
+
+
+
+
+
+
+
+      </div>
+
+
+    )
+  }
+}
+
+
+export default UserPage;
